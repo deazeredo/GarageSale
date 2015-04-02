0a4dcc74052be49532f212df647c317d65930bb7
deazeredo <deazeredo.lucas@gmail.com>
2015-04-02 23:30:46 +0000

    View files added, as well edit,delete, and update featuretures

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,4 +1,55 @@
 class GaragesController < ApplicationController
+  before_action :find_garage, only: [:show, :edit, :update, :destroy]
+
   def index    
+  end
+
+  def show
+    @garage = Garage.find(params[:id])
+    
+  end
+
+  def new
+    @garage = Garage.new
+  end
+
+  def create
+    @garage = Garage.new(garage_params)
+
+    if @garage.save
+      redirect_to @garage
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @garage.update(garage_params)
+      redirect_to @garage
+    else
+      render 'edit'
+      
+    end
+  end
+
+  def destroy
+    @garage.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def find_garage
+    @garage = Garage.find(params[:id])
+  end
+
+  def garage_params
+    params.require(:garage).permit(:title, :description, :address, :date, :time )
+    
   end
 end
